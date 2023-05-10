559c707986f4a71175106cb8072e95d63d1049e8
Anna Nguyen <anna.n668868@gmail.com>
2023-05-10 18:53:49 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import HomeAuth from "./pages/HomeAuth";
+import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+// export const apiUrl = process.env.API_URL;
 
 function App() {
+  const isAuth = Boolean(useSelector((state) => state.user.token));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+
+        <Route path="/home" element={<HomeAuth />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/login"
+          element={isAuth ? <Navigate to="/home" /> : <Login />}
+        />
+
+        <Route path="/createListing" element={<CreateListing />} />
+
+        <Route path="/listing/:id" element={<Listing />} />
+
+        <Route exact path="/cart" element={<Cart />} />
+
+        <Route exact path="/success" element={<Success />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
